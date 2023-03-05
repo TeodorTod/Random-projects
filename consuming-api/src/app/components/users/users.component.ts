49bb1b888ce0133ead5e0cff757fc37240aa5b63
Teodor Todorov <teodor.at.tod@gmail.com>
2023-03-05 23:54:56 +0200

    fetch from user service

--- consuming-api/src/app/components/users/users.component.ts
+++ consuming-api/src/app/components/users/users.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
 
+  constructor(private userService: UserService) {
+
+  }
+
+  ngOnInit(): void {
+    this.userService.getUsers(10).subscribe((res) => {
+      console.log(res)
+    })
+  }
 }
