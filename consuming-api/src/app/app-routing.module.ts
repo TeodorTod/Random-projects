ed08b49a5b2c254d4434316ac1a4d33acb0c52b8
Teodor Todorov <teodor.at.tod@gmail.com>
2023-03-05 22:15:39 +0200

    add random api

--- consuming-api/src/app/app-routing.module.ts
+++ consuming-api/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserdetailComponent } from './components/userdetail/userdetail.component';
+import { UsersComponent } from './components/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'users', component: UsersComponent},
+  {path: 'user/:uuid', component: UserdetailComponent},
+  {path: '**', redirectTo: 'users'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- consuming-api/src/app/app.module.ts
+++ consuming-api/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserdetailComponent } from './components/userdetail/userdetail.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserdetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
