e5e1a266d6bba5086ab1f625b2f7eaf9dd259852
Teodor Todorov <teodor.at.tod@gmail.com>
2023-03-07 22:19:50 +0200

    mapping users

--- consuming-api/src/app/interface/coordinate.inerface.ts
+++ consuming-api/src/app/interface/coordinate.inerface.ts
@@ -1,4 +1,4 @@
 export interface Coordinate {
     latitude: number;
-    logitute: number
+    longitude: number
 }
--- consuming-api/src/app/service/user.service.ts
+++ consuming-api/src/app/service/user.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Response } from '../interface/response.interface';
+import { User } from '../interface/user.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -19,4 +21,24 @@ export class UserService {
   getUser(uuid: number = 11): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/?uuid=${uuid}`)
   }
+
+  private processResponse(response: Response): Response {
+    return {
+      info: { ...response.info },
+      results: response.results.map((user: any) => (<User>{
+        uuid: user.login.uuid,
+        firstName: user.name.first,
+        lastName: user.name.last,
+        email: user.email,
+        username: user.login.username,
+        gender: user.gender,
+        address: `${user.location.street.number} ${user.location.street.name} ${user.location.city}, ${user.location.country}`,
+        dateOfBirth: user.dob.date,
+        phone: user.phone,
+        imageUrl: user.picture.medium,
+        coordinate: { latitude: +user.location.coordinates.latitude, longitude: +user.location.coordinates.longitude }
+      }))
+    };
+  }
+
 }
