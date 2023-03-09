43eee6816018df04a5c4fdf6d24216238ca433c0
Teodor Todorov <teodor.at.tod@gmail.com>
2023-03-09 22:48:21 +0200

    use detail form logic

--- consuming-api/src/app/components/userdetail/userdetail.component.ts
+++ consuming-api/src/app/components/userdetail/userdetail.component.ts
@@ -10,6 +10,8 @@ import { Response } from 'src/app/interface/response.interface';
 })
 export class UserdetailComponent implements OnInit {
   response: Response;
+  mode: 'edit' | 'locked' = 'locked';
+  buttonText: 'Edit' | 'Save' = 'Edit';
 
   constructor(private activatedRoute: ActivatedRoute, private userService: UserService) {
 
@@ -24,6 +26,15 @@ export class UserdetailComponent implements OnInit {
         
       })
     })
+  }
+
+  changeMode(mode?: 'edit' | 'locked'): void {
+    this.mode = this.mode === 'locked' ? 'edit' : 'locked';
+    this.buttonText = this.buttonText === 'Edit' ? 'Save' : 'Edit';
+    if (mode === 'edit') {
+      console.log('Updating user for the backend');
+      
+    }
   }
 
 
