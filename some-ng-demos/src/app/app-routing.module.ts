a728b4d1ea43101761a9c36601501694ee2e1d9e
Teodor Todorov <teodor.at.tod@gmail.com>
2023-04-02 22:40:26 +0300

    commit

--- some-ng-demos/src/app/app-routing.module.ts
+++ some-ng-demos/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerProfileComponentComponent } from './customer/customer-profile-component/customer-profile-component.component';
+import { ProfileGuard } from './guards/profile.guard';
 import { AboutComponent } from './pages/about/about.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { OurHomeComponent } from './pages/our-home/our-home.component';
@@ -8,7 +9,7 @@ import { OurHomeComponent } from './pages/our-home/our-home.component';
 const routes: Routes = [
   { path: 'home', component: OurHomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'user/:id', component: CustomerProfileComponentComponent },
+  { path: 'user/:id', component: CustomerProfileComponentComponent, canActivate: [ProfileGuard] },
   { path: '**', component: NotFoundComponent }
 
 ];
