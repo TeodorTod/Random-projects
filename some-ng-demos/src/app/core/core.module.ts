65804c1b91752f89d39fc625756040a9b7ada6d5
Teodor Todorov <teodor.at.tod@gmail.com>
2023-04-14 22:20:11 +0300

    commit

--- some-ng-demos/src/app/core/core.module.ts
+++ some-ng-demos/src/app/core/core.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Inject, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpInterceptor } from '@angular/common/http';
+import { TokenInterceptor } from './token.interceptor';
+import { ErrorHandlerInterceptor } from './error-handler.interceptor';
 
 @NgModule({
   declarations: [
@@ -15,6 +18,23 @@ import { FormsModule } from '@angular/forms';
   ],
   exports: [
     HeaderComponent
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorHandlerInterceptor,
+      multi: true
+    }
   ]
 })
-export class CoreModule { }
+export class CoreModule { 
+  constructor(@Inject(HTTP_INTERCEPTORS) private interceptors: HttpInterceptor[]) {
+    console.log(interceptors);
+    
+  }
+}
