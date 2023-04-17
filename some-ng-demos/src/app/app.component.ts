21d870225edced72cebd45d2af1a7b2afb1c47f5
Teodor Todorov <teodor.at.tod@gmail.com>
2023-04-17 23:21:22 +0300

    commit

--- some-ng-demos/src/app/app.component.ts
+++ some-ng-demos/src/app/app.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, Inject, Injectable } from '@angular/core';
+import { Component, Inject, Injectable, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -7,18 +7,30 @@ import { FormBuilder, Validators } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   registerForm = this.fb.group({
     username: ['', Validators.required],
     email: ['', [Validators.email, Validators.required]],
-    password: ['', [Validators.required]]
+    password: ['', [Validators.required]],
+    roleId: [1, Validators.required]
   })
+
+  roles = [
+    {id: 1, title: 'developer'},
+    {id: 2, title: 'qa'},
+  ]
 
   isSubmitted: boolean = false;
 
   constructor(private fb: FormBuilder) {
 
+  }
+
+  ngOnInit(): void {
+    this.registerForm.get('roleId')?.valueChanges.subscribe(roleId => {
+      console.log(roleId)
+    })
   }
 
   onSubmit() {
