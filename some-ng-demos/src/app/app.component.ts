6e8761c865752ed0055f9189a2208aeaa6107231
Teodor Todorov <teodor.at.tod@gmail.com>
2023-04-17 22:26:01 +0300

    commit

--- some-ng-demos/src/app/app.component.ts
+++ some-ng-demos/src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { DOCUMENT } from '@angular/common';
+
 import { Component, Inject, Injectable } from '@angular/core';
-import { Theme, ThemeService } from './core/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -8,35 +7,5 @@ import { Theme, ThemeService } from './core/theme.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'su-modules-and-routing';
 
-  constructor(@Inject(DOCUMENT) private document: Document, private themeService: ThemeService) {
-
-    this.themeService.onThemeChanges.subscribe(() => {
-      let themeIsDark = this.themeService.currentTheme === Theme.Dark;
-      if (themeIsDark) {
-        this.document.body.classList.remove('light-theme')
-        this.document.body.classList.add('dark-theme');
-      } else {
-        this.document.body.classList.remove('dark-theme')
-        this.document.body.classList.add('light-theme');
-      }
-    })
-
-  }
-
-}
-
-
-
-
-export abstract class Engine {
-}
-
-@Injectable()
-export class PetrolEngine extends Engine {
-}
-
-@Injectable()
-export class DieselEngine extends Engine {
 }
--- some-ng-demos/src/app/app.module.ts
+++ some-ng-demos/src/app/app.module.ts
@@ -4,35 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { CustomerModule } from './customer/customer.module';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { DefaultViewComponent } from './pages/default-view/default-view.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './pages/login/login.component';
-import { CustomForms } from './custom-forms/custom-forms.module';
-import { SharedModule } from './shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent,
-    DefaultViewComponent,
-    PageNotFoundComponent,
-    LoginComponent,
 
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    CoreModule,
     BrowserAnimationsModule,
-    CustomForms,
-    SharedModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
