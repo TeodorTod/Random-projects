cf1375dd4f5afcc18cce234d94e2495af4f63727
Teodor Todorov <teodor.at.tod@gmail.com>
2023-04-18 22:44:52 +0300

    reactive form

--- some-ng-demos/src/app/app.component.ts
+++ some-ng-demos/src/app/app.component.ts
@@ -1,12 +1,29 @@
 
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  reactiveForm = this.formBuilder.group({
+    firstname: '',
+    lastname: '',
+    email: '',
+    country: '',
+    gender: '',
+    hobbies: ''
+  })
 
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  onSubmit() {
+    console.log(this.reactiveForm.value);
+    
+  }
 
 }
