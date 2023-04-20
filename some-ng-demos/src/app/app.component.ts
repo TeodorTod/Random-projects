4fc73e7e4499f759c19f079787cabb77e7b1617a
Teodor Todorov <teodor.at.tod@gmail.com>
2023-04-20 23:03:38 +0300

    Operators of RxJS

--- some-ng-demos/src/app/app.component.ts
+++ some-ng-demos/src/app/app.component.ts
@@ -1,44 +1,66 @@
 
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, filter, find, from, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  
+export class AppComponent implements OnInit {
 
-  myObservable = new Observable((observer) => {
-    console.log('start');
-    setTimeout(() => {observer.next('1')}, 1000);
-    setTimeout(() => {observer.next('2')}, 2000);
-    setTimeout(() => {observer.next('3')}, 3000);
-    // setTimeout(() => {observer.error(new Error('Something went wrong'))}, 3000);
-    setTimeout(() => {observer.next('4')}, 4000);
-    setTimeout(() => {observer.next('5')}, 5000);
-    setTimeout(() => {observer.complete()}, 6000);
-    
- 
-  });
+
+  // myObservable = new Observable((observer) => {
+  //   console.log('start');
+  //   setTimeout(() => {observer.next('1')}, 1000);
+  //   setTimeout(() => {observer.next('2')}, 2000);
+  //   setTimeout(() => {observer.next('3')}, 3000);
+  //   // setTimeout(() => {observer.error(new Error('Something went wrong'))}, 3000);
+  //   setTimeout(() => {observer.next('4')}, 4000);
+  //   setTimeout(() => {observer.next('5')}, 5000);
+  //   setTimeout(() => {observer.complete()}, 6000);
+  // });
+
+  arr1 = [1, 2, 3, 4, 5];
+  arr2 = ['A', 'B', 'C', 'D', 'E'];
+
+
+  // myObservable = of(this.arr1, this.arr2);
+
+  myObservable = from(this.arr1);
+
+  transformedObs = this.myObservable.pipe(map((val) => {
+    return val * 5
+  }), filter((val) => {
+    return val >= 15
+  }), find((val) => {
+    return val % 5 == 0
+  }));
+
+
 
   constructor() {
 
   }
 
   ngOnInit(): void {
-    this.myObservable.subscribe(res => {
-      console.log(res)
-    }, (error) => {
-      alert(error.message)
-    }, () => {
-      alert('Observable has completed!')
+    //   this.transformedObs.subscribe(res => {
+    //     console.log(res)
+    //   }, (error) => {
+    //     alert(error.message)
+    //   }, () => {
+    //     alert('Observable has completed!')
+    //   })
+
+    this.transformedObs.subscribe({
+      next: (v) => console.log(v),
+      error: (e) => alert(e.message),
+      complete: () => alert('Observable has completed!')
     })
   }
 
- 
+
 
 
 }
