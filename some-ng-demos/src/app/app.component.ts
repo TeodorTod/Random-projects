1e58bafdbb77b5434448e02a62f8cf8ba6cf53da
Teodor Todorov <teodor.at.tod@gmail.com>
2023-04-20 21:52:32 +0300

    commit

--- some-ng-demos/src/app/app.component.ts
+++ some-ng-demos/src/app/app.component.ts
@@ -1,31 +1,44 @@
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  reactiveForm = this.formBuilder.group({
-    personalDetails: this.formBuilder.group({
-      firstname: ['', [Validators.required]],
-      lastname: ['', [Validators.required]],
-      email: ['', [Validators.email]],
-    }),
-    country: '',
-    gender: '',
-    hobbies: ''
-  })
+export class AppComponent implements OnInit{
+  
 
-  constructor(private formBuilder: FormBuilder) {
+  myObservable = new Observable((observer) => {
+    console.log('start');
+    setTimeout(() => {observer.next('1')}, 1000);
+    setTimeout(() => {observer.next('2')}, 2000);
+    setTimeout(() => {observer.next('3')}, 3000);
+    // setTimeout(() => {observer.error(new Error('Something went wrong'))}, 3000);
+    setTimeout(() => {observer.next('4')}, 4000);
+    setTimeout(() => {observer.next('5')}, 5000);
+    setTimeout(() => {observer.complete()}, 6000);
+    
+ 
+  });
+
+  constructor() {
 
   }
 
-  onSubmit() {
-    console.log(this.reactiveForm);
-
+  ngOnInit(): void {
+    this.myObservable.subscribe(res => {
+      console.log(res)
+    }, (error) => {
+      alert(error.message)
+    }, () => {
+      alert('Observable has completed!')
+    })
   }
+
+ 
+
 
 }
