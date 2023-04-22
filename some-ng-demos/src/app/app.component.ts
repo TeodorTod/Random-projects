441fd73ec192ae6e0294a66a80c033587559cc8d
Teodor Todorov <teodor.at.tod@gmail.com>
2023-04-22 22:35:18 +0300

    commit

--- some-ng-demos/src/app/app.component.ts
+++ some-ng-demos/src/app/app.component.ts
@@ -13,66 +13,8 @@ import { DataService } from './data.service';
 export class AppComponent implements OnInit {
 
 
-  // myObservable = new Observable((observer) => {
-  //   console.log('start');
-  //   setTimeout(() => {observer.next('1')}, 1000);
-  //   setTimeout(() => {observer.next('2')}, 2000);
-  //   setTimeout(() => {observer.next('3')}, 3000);
-  //   // setTimeout(() => {observer.error(new Error('Something went wrong'))}, 3000);
-  //   setTimeout(() => {observer.next('4')}, 4000);
-  //   setTimeout(() => {observer.next('5')}, 5000);
-  //   setTimeout(() => {observer.complete()}, 6000);
-  // });
-
-  arr1 = [1, 2, 3, 4, 5];
-  arr2 = ['A', 'B', 'C', 'D', 'E'];
-
-
-  // myObservable = of(this.arr1, this.arr2);
-
-  myObservable = from(this.arr1);
-
-  transformedObs = this.myObservable.pipe(map((val) => {
-    return val * 5
-  }), filter((val) => {
-    return val >= 15
-  }), find((val) => {
-    return val % 5 == 0
-  }));
-
-  counterObservable = interval(1000);
-  counterSub: any;
-
-
-  constructor(private data: DataService) {
-
-  }
-
-  ngOnInit(): void {
-    //   this.transformedObs.subscribe(res => {
-    //     console.log(res)
-    //   }, (error) => {
-    //     alert(error.message)
-    //   }, () => {
-    //     alert('Observable has completed!')
-    //   })
-
-    // this.transformedObs.subscribe({
-    //   next: (v) => console.log(v),
-    //   error: (e) => alert(e.message),
-    //   complete: () => alert('Observable has completed!')
-    // })
-
-    this.counterSub = this.counterObservable.subscribe((res) => {
-      console.log(res);
-      
-    })
-  }
-
-
-  unsubscribe() {
-    this.counterSub.unsubscribe();
-  }
-
+ngOnInit(): void {
+  
+}
 
 }
--- some-ng-demos/src/app/app.module.ts
+++ some-ng-demos/src/app/app.module.ts
@@ -6,15 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Comp1Component } from './comp1/comp1.component';
-import { Comp2Component } from './comp2/comp2.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    Comp1Component,
-    Comp2Component,
   ],
   imports: [
     BrowserModule,
