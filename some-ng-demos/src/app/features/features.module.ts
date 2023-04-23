19412e37eca3f507e6caef94dabe9928ea569a76
Teodor Todorov <teodor.at.tod@gmail.com>
2023-04-23 22:27:52 +0300

    commit

--- some-ng-demos/src/app/features/features.module.ts
+++ some-ng-demos/src/app/features/features.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContactInfoComponent } from './components/contact-info/contact-info.component';
+import { EducationComponent } from './components/education/education.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContactInfoComponent,
+    EducationComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ContactInfoComponent,
+    EducationComponent
   ]
 })
 export class FeaturesModule { }
