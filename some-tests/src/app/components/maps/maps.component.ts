c3d5fac82be19202b448a8de85fcfe6bb9f34424
Teodor Todorov <teodor.at.tod@gmail.com>
2023-07-16 22:47:58 +0300

    commit

--- some-tests/src/app/components/maps/maps.component.ts
+++ some-tests/src/app/components/maps/maps.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { BehaviorSubject, Subject, delay, from, map, mergeMap, of, switchMap } from 'rxjs';
+
+interface UserInterface {
+  id: string,
+  name: string
+}
 
 @Component({
   selector: 'app-maps',
@@ -6,5 +12,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./maps.component.scss']
 })
 export class MapsComponent {
+
+  foo$ = from([1,2,3,4,5,6]);
+
+  constructor() {
+    // this.foo$.subscribe(console.log)
+    const example = (operator: (source: any) => any) => () => {
+      from([1, 2, 3, 4, 5, 6])
+        .pipe(
+          operator((x: any) => of(x).pipe(delay(500)))
+        )
+        .subscribe({
+          next: console.log,
+          error: () => {},
+          complete: () => console.log(`${operator.name} completed`)
+        });
+    }
+    example(mergeMap)()
+    this.foo$.pipe(switchMap((x: any) => of(x).pipe(delay(1500))))
+    .subscribe(console.log)
+  }
 
 }
