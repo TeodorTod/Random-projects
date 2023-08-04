979ff1c7a5cc21925bdded9d953fabf5bb229703
Teodor Todorov <teodor.at.tod@gmail.com>
2023-08-04 22:59:33 +0300

    patchValue

--- mat-table/src/app/app.component.ts
+++ mat-table/src/app/app.component.ts
@@ -10,10 +10,25 @@ export class AppComponent {
   profileForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl('')
+    })
   });
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.profileForm.value);
+  }
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street'
+      }
+    });
   }
 }
