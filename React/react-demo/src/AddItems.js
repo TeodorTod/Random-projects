81f18811911b2be0dce446a0eced5ad1cc9571f5
Teodor Todorov <teodor.at.tod@gmail.com>
2023-10-07 20:58:15 +0300

    commit

--- React/react-demo/src/AddItems.js
+++ React/react-demo/src/AddItems.js
@@ -1,11 +1,15 @@
+import { useRef } from "react";
 import { FaPlus } from "react-icons/fa";
 
+
 const AddItems = ({ newItem, setNewItem, handleSubmit }) => {
+    const inputRef = useRef();
     return (
         <form className="addForm" onSubmit={handleSubmit}>
             <label htmlFor="addItem">Add Item</label>
             <input
                 autoFocus
+                ref={inputRef}
                 id="addItem"
                 type="text"
                 placeholder="Add new item"
@@ -13,7 +17,7 @@ const AddItems = ({ newItem, setNewItem, handleSubmit }) => {
                 value={newItem}
                 onChange={(e) => setNewItem(e.target.value)}
             />
-            <button type="submit" aria-label="Add Item">
+            <button type="submit" aria-label="Add Item" onClick={() => inputRef.current.focus()}>
                 <FaPlus />
             </button>
         </form>
