a9f628fab7fd477b018aa1344133dcd72c5480ae
Teodor Todorov <teodor.at.tod@gmail.com>
2023-10-12 22:35:13 +0300

    commit

--- React/test/src/App.js
+++ React/test/src/App.js
@@ -1,22 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Search from "./Search";
 import Value from "./Value";
 
-
 function App() {
-    const [searchValue, setSearchValue] = useState('')
+    const [searchValue, setSearchValue] = useState([]);
+    const [data, setData] = useState([]);
+    const [isButtonClicked, setIsButtonClicked] = useState(false);
+
+    useEffect(() => {
+        if (isButtonClicked) {
+            addUsers();
+        }
+    }, [isButtonClicked]);
+
+    const addUsers = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users/');
+        const resData = await response.json();
+        setData(resData);
+    }
+
+    const handleButtonClick = () => {
+        setIsButtonClicked(!isButtonClicked);
+    }
 
     return (
-        <main className="main">
-            <Value
-                searchValue={searchValue}
-                setSearchValue={setSearchValue}
-            />
-            <Search
-                searchValue={searchValue}
-                setSearchValue={setSearchValue}
-            />
-        </main>
+        <>
+            <button onClick={handleButtonClick}>Users</button>
+            <button>Posts</button>
+            <button>Comments</button>
+            {isButtonClicked && (
+                <ul>
+                    {data.map(user => (
+                        <li key={user.id}>{JSON.stringify(user)}</li>
+                    ))}
+                </ul>
+            )}
+            <main className="main">
+                <Value
+                    searchValue={searchValue}
+                    setSearchValue={setSearchValue}
+                />
+                <Search
+                    searchValue={searchValue}
+                    setSearchValue={setSearchValue}
+                />
+            </main>
+        </>
     );
 }
 
