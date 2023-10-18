ce1d7b7e2fc8ea9464b00354b20e13f92e9fc806
Teodor Todorov <teodor.at.tod@gmail.com>
2023-10-18 21:43:16 +0300

    commit

---
+++ React/blog/src/hooks/useAxiosFetch.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+
+const useAxiosFetch = (dataUrl) => {
+    const [data, setData] = useState([]);
+    const [fetchError, setFetchError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        let isMounted = true;
+        const source = axios.CancelToken.source();
+
+        const fetchData = async (url) => {
+            setIsLoading(true);
+            try {
+                const response = await axios.get(url, {
+                    cancelToken: source.token
+                });
+                if (isMounted) {
+                    setData(response.data);
+                    setFetchError(null)
+                }
+            } catch (error) {
+                if (isMounted) {
+                    setFetchError(error.message);
+                    setData([]);
+                }
+            } finally {
+                isMounted && setTimeout(() => {
+                    setIsLoading(false)
+                }, 2000)
+            }
+        };
+
+        fetchData(dataUrl);
+
+        const cleanUp = () => {
+            console.log('clean up function');
+            isMounted(false);
+            source.cancel();
+        }
+
+        return cleanUp;
+    }, [dataUrl]);
+
+    return { data, fetchError, isLoading }
+}
+
+export default useAxiosFetch;
