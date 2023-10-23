5d6a2ef790ff815c3326a3dd4505a1c4a135aae5
Teodor Todorov <teodor.at.tod@gmail.com>
2023-10-23 21:39:12 +0300

    commit

--- React/test/src/App.js
+++ React/test/src/App.js
@@ -1,22 +1,11 @@
-import { useState } from "react";
-import Demo from "./components/Demo";
-import { Context } from "./context"
-import Outside from "./components/Outside";
 
 
 function App() {
-    const [value, setValue] = useState(1) 
-    const changeValue = () => {
-        setValue(value + 1)
-    }
+ 
 
     return (
         <>
-        <Context.Provider value={value}>
-            <Demo />
-            <button onClick={changeValue}>Change</button>
-        </Context.Provider>
-        <Outside />
+      sdfsdf
     </>
     );
 }
