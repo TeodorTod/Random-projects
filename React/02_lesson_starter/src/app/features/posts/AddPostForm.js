dd392a870cf855a95f68c8b4792d29aa068fe344
Teodor Todorov <teodor.at.tod@gmail.com>
2023-10-26 22:14:46 +0300

    commit

--- React/02_lesson_starter/src/app/features/posts/AddPostForm.js
+++ React/02_lesson_starter/src/app/features/posts/AddPostForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { nanoid } from "@reduxjs/toolkit";
 import { postAdded } from "./postSlice";
+import { selectAllUsers } from "../users/usersSlice";
 
 
 
@@ -10,24 +10,31 @@ const AddPostForm = () => {
 
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
+    const [userId, setUserId] = useState('')
 
+    const users = useSelector(selectAllUsers)
 
     const onTitleChanged = e => setTitle(e.target.value)
     const onContentChanged = e => setContent(e.target.value)
+    const onAuthorChanged = e => setUserId(e.target.value)
 
     const onSavePostClicked = () => {
         if (title && content) {
             dispatch(
-                postAdded({
-                    id: nanoid(),
-                    title,
-                    content
-                })
+                postAdded(title, content, userId)
             )
             setTitle('')
             setContent('')
         }
     }
+
+    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+
+    const usersOptions = users.map(user => (
+        <option key={user.id} value={user.id}>
+            {user.name}
+        </option>
+    ))
 
     return (
         <section>
@@ -41,6 +48,11 @@ const AddPostForm = () => {
                     value={title}
                     onChange={onTitleChanged}
                 />
+                <label htmlFor="postAuthor">Author:</label>
+                <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+                    <option value=""></option>
+                    {usersOptions}
+                </select>
                 <label htmlFor="postContent">Content:</label>
                 <textarea
                     id="postContent"
@@ -51,6 +63,7 @@ const AddPostForm = () => {
                 <button
                     type="button"
                     onClick={onSavePostClicked}
+                    disabled={!canSave}
                 >Save Post</button>
             </form>
         </section>
--- React/02_lesson_starter/src/app/store.js
+++ React/02_lesson_starter/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postReducer from "./features/posts/postSlice";
+import usersReducer from "./features/users/usersSlice";
 
 
 export const store = configureStore({
     reducer: {
-        posts: postReducer
+        posts: postReducer,
+        users: usersReducer
     }
 })
