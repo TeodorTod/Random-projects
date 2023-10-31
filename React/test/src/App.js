c739a35f9431c31483b24b0a9260af4b1d8b49f2
TeodorTod <teodor.at.tod@gmail.com>
2023-10-31 21:03:44 +0200

    commit

--- React/test/src/App.js
+++ React/test/src/App.js
@@ -1,13 +1,12 @@
-import Counter from "./app/features/counter/Counter";
 
 
 function App() {
 
 
     return (
-        <main className="App">
-            <Counter />
-        </main>
+        <>
+            sdf
+        </>
     );
 }
 
