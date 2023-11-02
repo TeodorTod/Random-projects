24ff4ac89237225412392f16492321e63e985516
TeodorTod <teodor.at.tod@gmail.com>
2023-11-02 22:14:17 +0200

    commit

---
+++ React/test/src/features/cart/cartSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+import cartItems from '../../cartItems';
+
+const initialState = {
+    cartItems: cartItems,
+    amount: 1,
+    total: 0,
+    isLoading: true
+}
+
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+
+})
+
+
+export default cartSlice.reducer;
