191c39f1ed114d6a9f63e0352f083714e1781f3d
TeodorTod <teodor.at.tod@gmail.com>
2023-11-11 23:20:33 +0200

    commit

--- ng-16-chart/src/app/chart/chart.component.ts
+++ ng-16-chart/src/app/chart/chart.component.ts
@@ -11,6 +11,7 @@ Chart.register(...registerables);
 export class ChartComponent implements AfterViewInit {
   @ViewChild('chart') chartRef!: ElementRef<HTMLCanvasElement>;
   chart!: Chart;
+  datasetVisibility: boolean[] = [true, true, true]; // Initially, all datasets are visible
 
   ngAfterViewInit() {
     this.createChart();
@@ -19,23 +20,38 @@ export class ChartComponent implements AfterViewInit {
   createChart() {
     const context = this.chartRef.nativeElement.getContext('2d');
     if (context) {
-      // Create a green gradient
-      const gradient = context.createLinearGradient(0, 0, 0, 400);
-      gradient.addColorStop(0, '#98FB98'); // Light green at the top
-      gradient.addColorStop(1, '#006400'); // Dark green at the bottom
+      const gradient = this.createGreenGradient(context);
 
       this.chart = new Chart(context, {
         type: 'line',
         data: {
           labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-          datasets: [{
-            label: 'Sample Dataset',
-            data: [65, 59, 80, 81, 56, 55, 40],
-            fill: true,
-            borderColor: 'rgb(75, 192, 192)',
-            backgroundColor: gradient, // Using the gradient
-            tension: 0.1
-          }]
+          datasets: [
+            {
+              label: 'Dataset 1',
+              data: [65, 59, 80, 81, 56, 55, 40],
+              fill: true,
+              borderColor: 'rgb(75, 192, 192)',
+              backgroundColor: gradient,
+              tension: 0.1
+            },
+            {
+              label: 'Dataset 2',
+              data: [28, 48, 40, 19, 86, 27, 90],
+              fill: true,
+              borderColor: 'rgb(192, 75, 75)',
+              backgroundColor: gradient,
+              tension: 0.1
+            },
+            {
+              label: 'Dataset 3',
+              data: [18, 48, 77, 9, 100, 27, 40],
+              fill: true,
+              borderColor: 'rgb(75, 75, 192)',
+              backgroundColor: gradient,
+              tension: 0.1
+            }
+          ]
         },
         options: {
           responsive: true,
@@ -53,5 +69,18 @@ export class ChartComponent implements AfterViewInit {
         }
       });
     }
+  }
+
+  createGreenGradient(context: CanvasRenderingContext2D) {
+    const gradient = context.createLinearGradient(0, 0, 0, 400);
+    gradient.addColorStop(0, '#98FB98'); // Light green at the top
+    gradient.addColorStop(1, '#006400'); // Dark green at the bottom
+    return gradient;
+  }
+
+  toggleDataset(datasetIndex: number) {
+    this.datasetVisibility[datasetIndex] = !this.datasetVisibility[datasetIndex];
+    this.chart.getDatasetMeta(datasetIndex).hidden = !this.chart.getDatasetMeta(datasetIndex).hidden;
+    this.chart.update();
   }
 }
