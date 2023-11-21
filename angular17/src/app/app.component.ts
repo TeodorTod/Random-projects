8712987e65b2a9a40b5550cfa5c28a57fc55ebad
TeodorTod <teodor.at.tod@gmail.com>
2023-11-21 21:42:32 +0200

    commit

--- angular17/src/app/app.component.ts
+++ angular17/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { test } from './interfaces/test.interface';
+import { filter, map, of } from 'rxjs';
+import { fail } from 'assert';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,23 @@ import { test } from './interfaces/test.interface';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular17';
+
+  users = [
+    { id: 1, name: 'John', isActive: true },
+    { id: 2, name: 'Pencho', isActive: true },
+    { id: 3, name: 'Stancho', isActive: false },
+  ];
+
+  users$ = of(this.users);
+  usernames$ = this.users$.pipe(map((users) => users.map((user) => user.name)));
+  filteredusers$ = this.users$.pipe(filter((users) => users.every(user => user.isActive)))
+
+  ngOnInit(): void {
+    // this.users$.subscribe(users => {
+    //   console.log(users);
+    // })
+  }
 
 }
--- angular17/src/app/services/app.service.ts
+++ angular17/src/app/services/app.service.ts
@@ -1,13 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class AppService {
+export class AppService  {
 
-  constructor() { }
-
-  private categorySubject = new Subject();
-  categorySubject$ = this.categorySubject.asObservable();
 }
