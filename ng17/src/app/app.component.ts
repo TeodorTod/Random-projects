91e9e30b6270697f9a1a0a01348a439d5d253ac0
TeodorTod <teodor.at.tod@gmail.com>
2023-11-24 22:49:25 +0200

    commit

--- ng17/src/app/app.component.ts
+++ ng17/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -10,5 +10,21 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng17';
+  counter = signal(0);
+  numbers = signal([2, 4, 6])
+  sumOfNumbers = computed(() => this.numbers().map((el) => el * 2))
+
+  increment() {
+    this.counter.update((el) => el + 1)
+  }
+
+  constructor() {
+    effect(() => {
+      if (this.counter() > 10) {
+        alert('Over 10')
+      }
+    })
+  }
+
+
 }
