76cfc303164be71fed0e48b68ab7fb99c82d53eb
TeodorTod <teodor.at.tod@gmail.com>
2023-11-25 23:03:00 +0200

    commit

--- ng-16/src/app/app.component.ts
+++ ng-16/src/app/app.component.ts
@@ -1,42 +1,17 @@
 import { Component, OnInit, computed, effect, inject, signal } from '@angular/core';
-import { AppService } from './app.service';
 
-interface UserInterface {
-  id: string,
-  name: string
-}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  title = signal('');
-  users = signal<UserInterface[]>([]);
-  appService = inject(AppService);
+export class AppComponent {
 
-  usersChangeEffect = effect(() => {
-    console.log(this.users().length);
+
+
+  logout() {
+    console.log('logout');
     
-  })
-
-  usersTotal = computed(() => this.users().length)
-
-  ngOnInit(): void {
-    this.appService.showNames();
-    
-    setTimeout(() => {
-      // this.users.set([{id: '1', name: 'Gosho'}]);
-      this.users.update(prevUsers => [...prevUsers, {id: '1', name: 'Gosho'}])
-      console.log(this.users());
-      
-    }, 112000)
-  }
-
-  changeTitle(event: Event) {
-    const title = (event.target as HTMLInputElement).value;
-
-    this.title.set(title)
   }
 }
--- ng-16/src/app/app.module.ts
+++ ng-16/src/app/app.module.ts
@@ -3,22 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
-import { DemoComponent } from './demo/demo.component';
-import { FormsModule } from '@angular/forms';
-import { TestComponent } from './test/test.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent,
-    DemoComponent,
-    TestComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule      
   ],
   providers: [],
   bootstrap: [AppComponent]
