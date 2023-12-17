541bc87f2ffcdedeeb7d78c6d8b763148a009ccc
TeodorTod <teodor.at.tod@gmail.com>
2023-12-17 21:55:39 +0200

    commit

--- change-detection/src/app/app.component.ts
+++ change-detection/src/app/app.component.ts
@@ -9,34 +9,5 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  name = 'JS Frontend'
-  title = 'change-detection';
-  topicName = 'Change-detection in Angular';
-  isVisible = true;
-  createdDate = new Date();
-  subject = new BehaviorSubject(0);
-  subscription: any;
 
-  constructor() {
-    this.subscription = interval(1000).subscribe(() => {
-      const currentValue = this.subject.getValue();
-      this.subject.next(currentValue + 1);
-      console.log(this.subject.value);
-    });
-    setTimeout(() => {
-      this.subscription.pipe(
-        takeUntilDestroyed()
-      )
-    }, 5000)
-  }
-
-
-
-  getInfo() {
-    return '1M Views'
-  }
-
-  changeTopic() {
-    this.topicName = 'New Topic Name'
-  }
 }
--- change-detection/src/app/app.module.ts
+++ change-detection/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChanelComponent } from './chanel/chanel.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    ChanelComponent
   ],
   imports: [
     BrowserModule,
