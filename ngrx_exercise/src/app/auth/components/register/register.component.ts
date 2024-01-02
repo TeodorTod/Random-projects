7feaf367446b8b8137db57cd66880403c341731c
TeodorTod <teodor.at.tod@gmail.com>
2024-01-02 22:23:37 +0200

    commit

---
+++ ngrx_exercise/src/app/auth/components/register/register.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'mc-register',
+    templateUrl: './register.component.html',
+    standalone: true,
+    imports: []
+})
+
+export class RegisterComponent {
+
+}
--- ngrx_exercise/src/main.ts
+++ ngrx_exercise/src/main.ts
@@ -1,4 +1,6 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
+import { provideRouter } from "@angular/router";
+import { appRoutes } from "./app/app.routes";
 
-bootstrapApplication(AppComponent, {providers: []})
+bootstrapApplication(AppComponent, {providers: [provideRouter(appRoutes)]})
