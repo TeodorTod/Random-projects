60f182fb6532ef85592f16649ff5743d4cc06ca7
TeodorTod <teodor.at.tod@gmail.com>
2024-02-01 22:52:23 +0200

    also sessions

--- Express/src/index.mjs
+++ Express/src/index.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import routes from './routes/index.mjs';
 import cookieParser from "cookie-parser";
 import session from "express-session";
+import { mockUsers } from "./utils/constants.mjs";
 
 const app = express();
 app.use(express.json());
@@ -24,8 +25,46 @@ app.get("/", (req, res) => {
     console.log(req.session);
     console.log(req.session.id);
     req.session.visited = true;
-    res.cookie('hello', 'world', {maxAge: 60000 * 60, signed: true});
+    res.cookie('hello', 'world', { maxAge: 60000 * 60, signed: true });
     res.send('hello')
+});
+
+app.post('/api/auth', (req, res) => {
+    const {
+        body: { username, password }
+    } = req;
+    const findUser = mockUsers.find((user) => user.username === username);
+    if (!findUser || findUser.password !== password) {
+        return res.status(401).send({ msg: 'Bad credentials!' });
+    }
+    req.session.user = findUser;
+    return res.status(200).send(findUser);
+});
+
+app.get('/api/auth/status', (req, res) => {
+    req.sessionStore.get(req.sessionID, (err, session) => {
+        console.log(session);
+    });
+    req.session.user ? res.status(200).send(req.session.user) : res.status(401).send({ msg: 'Not authenticated!' });
+});
+
+app.post('/api/cart', (req, res) => {
+    if (!req.session.user) return res.sendStatus(401);
+    const { body: item } = req;
+    const { cart } = req.session;
+
+    if (cart) {
+        cart.push(item);
+    } else {
+        req.session.cart = [item];
+    }
+
+    return res.status(201).send(item);
+});
+
+app.get('/api/cart', (req, res) => {
+    if (!req.session.user) return res.sendStatus(401);
+    return res.send(req.session.cart ?? [])
 });
 
 
--- Express/src/utils/constants.mjs
+++ Express/src/utils/constants.mjs
@@ -1,10 +1,10 @@
 export let mockUsers = [
-    { id: 1, username: "Pesho", displayName: "Pesho" },
-    { id: 2, username: "Gincho", displayName: "Gincho" },
-    { id: 3, username: "Stancho", displayName: "Stancho" },
-    { id: 4, username: "Kyncho", displayName: "Kyncho" },
-    { id: 5, username: "Yovko", displayName: "Yovko" },
-    { id: 6, username: "Aron", displayName: "Aron" },
-    { id: 7, username: "Boris", displayName: "Boris" },
+    { id: 1, username: "Pesho", displayName: "Pesho", password: 'test923' },
+    { id: 2, username: "Gincho", displayName: "Gincho", password: 'test123' },
+    { id: 3, username: "Stancho", displayName: "Stancho", password: 'test123' },
+    { id: 4, username: "Kyncho", displayName: "Kyncho", password: 'test323' },
+    { id: 5, username: "Yovko", displayName: "Yovko", password: 'test1223' },
+    { id: 6, username: "Aron", displayName: "Aron", password: 'test123' },
+    { id: 7, username: "Boris", displayName: "Boris", password: 'test623' },
 ];
 
