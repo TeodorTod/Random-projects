a0901d786d05ddb52f74505262d7f73bf50c7dfa
TeodorTod <teodor.at.tod@gmail.com>
2024-02-08 22:25:55 +0200

    commit

--- Express/app.js
+++ Express/app.js
@@ -1,23 +1,18 @@
-const Circle = require('./circle');
-const circle = new Circle;
 
 
-console.log(circle.area(5));
-console.log(circle.circumference(5));
+
+const http = require('node:http');
 
 
-// const http = require('node:http');
+// Create a local server to receive data from
+const server = http.createServer();
 
+// Listen to the request event
+server.on('request', (request, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    data: 'Hello World!',
+  }));
+});
 
-// // Create a local server to receive data from
-// const server = http.createServer();
-
-// // Listen to the request event
-// server.on('request', (request, res) => {
-//   res.writeHead(200, { 'Content-Type': 'application/json' });
-//   res.end(JSON.stringify({
-//     data: 'Hello World!',
-//   }));
-// });
-
-// server.listen(3000);
+server.listen(3000);
