1fa7443b62634cc1fa3c3923f2ced77decf41321
TeodorTod <teodor.at.tod@gmail.com>
2024-02-09 22:54:06 +0200

    routes

--- Express/app.js
+++ Express/app.js
@@ -1,18 +1,46 @@
-
-
-
-const http = require('node:http');
-
+const http = require("node:http");
+const fs = require("node:fs");
 
 // Create a local server to receive data from
 const server = http.createServer();
 
 // Listen to the request event
-server.on('request', (request, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({
-    data: 'Hello World!',
-  }));
+
+server.on("request", (request, res) => {
+  if (request.url === "/") {
+    fs.readFile("page/home.html", "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else if (request.url === "/about") {
+    fs.readFile("page/about.html", "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else {
+    fs.readFile("page/404.html", "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+        return;
+      }
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  }
 });
 
 server.listen(3000);
