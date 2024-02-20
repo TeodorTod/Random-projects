aad82d16b71891f4ac79cdcb92f3a9e07f8a571f
TeodorTod <teodor.at.tod@gmail.com>
2024-02-20 22:21:32 +0200

    commit

---
+++ angular-testing/src/app/shared/utils/utils.spec.ts
@@ -0,0 +1,5 @@
+describe('utils', () => {
+    it('some name of the test', () => {
+        expect(1).toEqual(1)
+    })
+})
---
+++ angular-testing/src/app/shared/utils/utils.ts
@@ -0,0 +1,6 @@
+export const range = (start: number, end: number): number[] => {
+    return [...Array(end - start).keys()].map((el) => el + start);
+};
+export const pluck = (elements: any[], field: string) => {
+    return elements.map((el) => el[field]);
+};
