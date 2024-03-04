71388ceb5a043bc4500ec21f7a826d1165bf4590
TeodorTod <teodor.at.tod@gmail.com>
2024-03-04 22:19:56 +0200

    commit

---
+++ express-auth/src/index.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+    credentials: true
+}));
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+    console.log(`Server os listening on port 3000`);
+    
+})
+
+
