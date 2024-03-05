7bf770109113a8dea889ec3eec86938403673140
TeodorTod <teodor.at.tod@gmail.com>
2024-03-05 20:35:13 +0200

    commit

--- express-auth/src/index.ts
+++ express-auth/src/index.ts
@@ -4,6 +4,8 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import router from './router';
 
 const app = express();
 
@@ -18,7 +20,16 @@ const server = http.createServer(app);
 
 server.listen(3000, () => {
     console.log(`Server os listening on port 3000`);
-    
+
 })
 
+const MONGO_URL = 'mongodb+srv://teodor:teodor@cluster0.icwl4jm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => {
+    console.log(error);
+
+});
+
+app.use('/', router());
