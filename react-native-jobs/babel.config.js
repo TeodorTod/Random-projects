6b90fdbd0405879a4f35e4314111d9b768f276df
TeodorTod <teodor.at.tod@gmail.com>
2024-03-19 23:18:49 +0200

    commit

--- react-native-jobs/babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      // Remove 'expo-router/babel' and include any other plugins you might need
-    ],
-  };
-};
