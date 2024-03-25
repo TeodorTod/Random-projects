84c25cfdbb5c6824b91ec3d18b36f0d3415ac3c4
TeodorTod <teodor.at.tod@gmail.com>
2024-03-25 16:12:57 +0200

    fix styles

--- react-native-demo/App.tsx
+++ react-native-demo/App.tsx
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+type ValueProps = {
+  label: string,
+  value: string
+}
+
+const Value = ({ label, value }: ValueProps) => (
+  <View>
+    <Text style={styles.label}>{label}</Text>
+    <Text style={styles.value}>{value}</Text>
+  </View>
+);
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello World!</Text>
+
+      <View style={styles.values}>
+        <Value label="Steps" value="1914" />
+        <Value label="Distance" value="0.45 km" />
+      </View>
+
+      <Value label="Flights Climbed" value="15" />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'black',
     justifyContent: 'center',
+    padding: 12
+  },
+  values: {
+    flexDirection: 'row',
+    gap: 25,
+    flexWrap: 'wrap',
+  },
+  label: {
+    color: 'white',
+    fontSize: 20
+  },
+  value: {
+    fontSize: 35,
+    color: '#AFB3BE',
+    fontWeight: '500',
+
   },
 });
