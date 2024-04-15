8b6e400b5d5027c4be3cf1292316686198652b61
TeodorTod <teodor.at.tod@gmail.com>
2024-04-15 22:43:05 +0300

    commit

--- react-native-demo/src/components/RingProgress.tsx
+++ react-native-demo/src/components/RingProgress.tsx
@@ -1,23 +1,44 @@
 import { View, Text } from "react-native";
-import Svg, { Circle, Rect } from 'react-native-svg';
+import Svg, { Circle, Rect } from "react-native-svg";
 
 type RingProgressProps = {
-    radius?: number,
-    strokeWidth?: number,
+  radius?: number;
+  strokeWidth?: number;
 };
 
-const color = "#EE0F55"
+const color = "#EE0F55";
 
-const RingProgress = ({ radius = 100, strokeWidth = 15 }: RingProgressProps) => {
-    const innnerRadius = radius - strokeWidth -20;
-    return (
-        <View style={{ width: radius * 2, height: radius * 2, alignSelf: "center",}}>
-            <Svg>
-                <Circle cx={radius} cy={innnerRadius +10} r={innnerRadius} strokeWidth={strokeWidth} stroke={color}/>
-            </Svg>
-        </View>
-
-    );
-}
+const RingProgress = ({
+  radius = 100,
+  strokeWidth = 20,
+}: RingProgressProps) => {
+  const innnerRadius = radius - strokeWidth - 20;
+  const circumference = 2 * Math.PI * innnerRadius;
+  return (
+    <View
+      style={{ width: radius * 2, height: radius * 2, alignSelf: "center" }}
+    >
+      <Svg>
+        <Circle
+          cx={radius}
+          cy={innnerRadius + 10}
+          r={innnerRadius}
+          strokeWidth={strokeWidth}
+          stroke={color}
+          opacity={0.3}
+        />
+        <Circle
+          cx={radius}
+          cy={innnerRadius + 10}
+          r={innnerRadius}
+          strokeWidth={strokeWidth}
+          stroke={color}
+          strokeDasharray={[circumference * 0.2, circumference]}
+          strokeLinecap="round"
+        />
+      </Svg>
+    </View>
+  );
+};
 
 export default RingProgress;
