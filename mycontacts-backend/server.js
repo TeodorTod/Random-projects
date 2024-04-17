5b103b4d98d348ecb6442d76e7c051a7c8bb1d0c
TeodorTod <teodor.at.tod@gmail.com>
2024-04-17 21:51:05 +0300

    initial config

---
+++ mycontacts-backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const dotenv = require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.json({msg: 'Hello World!'})
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
