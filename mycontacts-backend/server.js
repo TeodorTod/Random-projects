27cf9a51c115992c17864b7969e03c9b46db7926
TeodorTod <teodor.at.tod@gmail.com>
2024-04-20 22:56:21 +0300

    mongoDB connection

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const errorHandler = require('./middleware/errorHandler');
+const connectDB = require('./config/dbConnection');
 const dotenv = require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 3000;
 
+connectDB();
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App listening on port ${port}`)
 });
