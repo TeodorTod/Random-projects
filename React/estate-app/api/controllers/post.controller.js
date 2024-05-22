307f14a1ac31528b9321bf1a3156dc70645d41ca
TeodorTod <teodor.at.tod@gmail.com>
2024-05-22 21:59:38 +0300

    change prisma models and others

--- React/estate-app/api/controllers/post.controller.js
+++ React/estate-app/api/controllers/post.controller.js
@@ -2,50 +2,83 @@ import prisma from "../lib/prisma.js";
 
 export const getPosts = async (req, res) => {
     try {
-        
-        res.status(200).json({})
+        const posts = await prisma.post.findMany();
+        res.status(200).json(posts);
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: 'Failed to get posts' })
+        res.status(500).json({ message: 'Failed to get posts' });
     }
 };
 
 export const singlePost = async (req, res) => {
+    const id = req.params.id;
     try {
-        
-        res.status(200).json({})
+        const post = await prisma.post.findUnique({
+            where: { id },
+            include: {
+                postDetail: true,
+                user: {
+                    select: {
+                        username: true,
+                        avatar: true,
+                    },
+                },
+            },
+        });
+        res.status(200).json(post)
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: 'Failed to get post' })
+        res.status(500).json({ message: 'Failed to get post' });
     }
 };
 
 export const addPost = async (req, res) => {
+    const body = req.body;
+    const tokenUserId = req.userId;
     try {
-        
-        res.status(200).json({})
+        const newPost = await prisma.post.create({
+            data: {
+                ...body.postData,
+                userId: tokenUserId,
+                postDetail: {
+                    create: body.postDetail
+                }
+            }
+        })
+        res.status(200).json(newPost);
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: 'Failed to add post' })
+        res.status(500).json({ message: 'Failed to add post' });
     }
 };
 
 export const updatePost = async (req, res) => {
     try {
-        
-        res.status(200).json({})
+
+        res.status(200).json({});
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: 'Failed to update post' })
+        res.status(500).json({ message: 'Failed to update post' });
     }
 };
 
 export const deletePost = async (req, res) => {
+    const id = req.params.id;
+    const tokenUserId = req.userId;
+
     try {
-        
-        res.status(200).json({})
+        const post = await prisma.post.findUnique({
+            where: { id }
+        });
+        if (post.userId !== tokenUserId) {
+            return res.status(403).json({ message: "Not Authorized" })
+        }
+        await prisma.post.delete({
+            where: { id }
+        })
+        res.status(200).json({ message: "Post deleted!" });
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: 'Failed to delete post' })
+        res.status(500).json({ message: 'Failed to delete post' });
     }
 };
--- React/estate-app/client/src/App.jsx
+++ React/estate-app/client/src/App.jsx
@@ -10,6 +10,7 @@ import ProfilePage from "./routes/profilePage/profilePage";
 import Login from "./routes/login/login";
 import Register from "./routes/register/register";
 import ProfileUpdatePage from "./routes/profileUpdatePage/profileUpdatePage";
+import NewPostPage from "./routes/newPostPage/newPostPage";
 
 function App() {
   const router = createBrowserRouter([
@@ -50,6 +51,10 @@ function App() {
         {
           path: "/profile/update",
           element: <ProfileUpdatePage />
+        },
+        {
+          path: "/add",
+          element: <NewPostPage />
         },
       ]
     }
