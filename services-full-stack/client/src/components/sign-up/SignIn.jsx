ec265ac25e59e9b6140a9aa8f9ba5fbfa5a950ff
TeodorTod <teodor.at.tod@gmail.com>
2024-06-16 22:04:54 +0300

    sign in FE

--- services-full-stack/client/src/components/sign-up/SignIn.jsx
+++ services-full-stack/client/src/components/sign-up/SignIn.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import IconButton from '@mui/material/IconButton';
+import InputLabel from '@mui/material/InputLabel';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import Button from '@mui/material/Button';
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <>
+            <h1>Sign in here</h1>
+            <Box sx={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'column', alignItems: 'center' }}>
+                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                    <TextField
+                        label="Email"
+                        id="email"
+                        placeholder='Write your email here'
+                        fullWidth
+                    />
+                </FormControl>
+                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                    <InputLabel htmlFor="password">Password</InputLabel>
+                    <OutlinedInput
+                        id="password"
+                        type={showPassword ? 'text' : 'password'}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="Password"
+                    />
+                </FormControl>
+                <Button variant="contained" sx={{ m: 1, width: '250px' }}>
+                    Sign In
+                </Button>
+            </Box>
+        </>
+    );
 }
 
-export default SignIn
+export default SignIn;
