be0c46544a68f8b83f615831429a36a996efd11b
TeodorTod <teodor.at.tod@gmail.com>
2024-06-18 23:09:51 +0300

    sign up connect to backend

--- services-full-stack/client/src/components/sign-in/SignUp.jsx
+++ services-full-stack/client/src/components/sign-in/SignUp.jsx
@@ -1,9 +1,97 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import IconButton from '@mui/material/IconButton';
+import InputLabel from '@mui/material/InputLabel';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import Button from '@mui/material/Button';
+import axios from 'axios';
+import apiRequest from '../../lib/apiRequest';
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+    const [showPassword, setShowPassword] = useState(false);
+    const [formData, setFormData] = useState({ email: '', username: '', password: '' });
+    const navigate = useNavigate();
 
-export default SignUp
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await apiRequest.post('/auth/register', formData);
+            navigate('/signIn')
+        } catch (error) {
+            console.error('Error registering user:', error);
+        }
+    };
+
+    return (
+        <>
+            <h1>Sign up here</h1>
+            <form onSubmit={handleSubmit}>
+                <Box sx={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'column', alignItems: 'center' }}>
+                    <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                        <TextField
+                            label="Email"
+                            id="email"
+                            placeholder='Write your email here'
+                            fullWidth
+                            value={formData.email}
+                            onChange={handleChange}
+                        />
+                    </FormControl>
+                    <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                        <TextField
+                            label="Username"
+                            id="username"
+                            placeholder='Write your username here'
+                            fullWidth
+                            value={formData.username}
+                            onChange={handleChange}
+                        />
+                    </FormControl>
+                    <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                        <InputLabel htmlFor="password">Password</InputLabel>
+                        <OutlinedInput
+                            id="password"
+                            type={showPassword ? 'text' : 'password'}
+                            value={formData.password}
+                            onChange={handleChange}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                    >
+                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            label="Password"
+                        />
+                    </FormControl>
+                    <Button type="submit" variant="contained" sx={{ m: 1, width: '25ch' }}>
+                        Register
+                    </Button>
+                </Box>
+            </form>
+        </>
+    );
+};
+
+export default SignUp;
--- services-full-stack/server/server.js
+++ services-full-stack/server/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const connectDB = require('./config/dbConnection');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 connectDB();
 app.use(express.json());
 app.use("/api/auth", require("./routes/authRoute"));
