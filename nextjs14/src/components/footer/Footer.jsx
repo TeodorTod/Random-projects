dba9fdb4d48b822675dd3edf7cf9e2dc21f1a1dc
TeodorTod <teodor.at.tod@gmail.com>
2024-08-23 21:22:44 +0300

    design few pages

--- nextjs14/src/components/footer/Footer.jsx
+++ nextjs14/src/components/footer/Footer.jsx
@@ -1,8 +1,14 @@
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.logo}>lamadev</div>
+      <div className={styles.text}>
+        Lama creative thoughts agency © All rights reserved.
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
