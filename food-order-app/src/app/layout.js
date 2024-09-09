074670cda0e0cd0a8a7db149d0253a94b99b20e9
TeodorTod <teodor.at.tod@gmail.com>
2024-09-09 21:47:11 +0300

    header fixes

--- food-order-app/src/app/layout.js
+++ food-order-app/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "../components/layout/Header";
+import { AppProvider } from '@/components/AppContext'
 
 
 const roboto = Roboto({
@@ -18,11 +19,13 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={roboto.className}>
         <main className="max-w-4xl mx-auto p-4">
-          <Header />
-          {children}
-          <footer className="border-t p-8 text-center text-gray-500 mt-16">
-            &copy; 2024 All rights reserved
-          </footer>
+          <AppProvider>
+            <Header />
+            {children}
+            <footer className="border-t p-8 text-center text-gray-500 mt-16">
+              &copy; 2024 All rights reserved
+            </footer>
+          </AppProvider>
         </main>
       </body>
     </html>
--- food-order-app/src/components/layout/Header.js
+++ food-order-app/src/components/layout/Header.js
@@ -1,6 +1,12 @@
+"use client";
+
+import { useSession, signOut } from "next-auth/react";
 import Link from "next/link";
 
 const Header = () => {
+    const session = useSession();
+    const status = session.status;
+
     return (
         <header className="flex items-center justify-between">
             <nav className="flex gap-8 text-gray-500 font-semibold items-center">
@@ -11,13 +17,27 @@ const Header = () => {
                 <Link href={"/"}>Menu</Link>
                 <Link href={"/"}>About</Link>
                 <Link href={"/"}>Contact</Link>
-
             </nav>
             <nav className="flex items-center gap-4 text-gray-500 font-semibold">
-                <Link href={'/login'}>Login</Link>
-                <Link href={'/register'} className="bg-primary text-white px-6 py-2 rounded-full">
-                    Register
-                </Link>
+                {status === "authenticated" && (
+                    <button
+                       onClick={() => signOut()}
+                        className="bg-primary text-white px-6 py-2 rounded-full"
+                    >
+                        Logout
+                    </button>
+                )}
+                {status == "unauthenticated" && (
+                    <>
+                        <Link href={"/login"}>Login</Link>
+                        <Link
+                            href={"/register"}
+                            className="bg-primary text-white px-6 py-2 rounded-full"
+                        >
+                            Register
+                        </Link>
+                    </>
+                )}
             </nav>
         </header>
     );
