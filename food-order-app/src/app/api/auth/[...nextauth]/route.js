58e2ad4af8521aa60f0fe3d1e4eb406daac5ca8b
TeodorTod <teodor.at.tod@gmail.com>
2024-09-13 21:55:57 +0300

    commit

--- food-order-app/src/app/api/auth/[...nextauth]/route.js
+++ food-order-app/src/app/api/auth/[...nextauth]/route.js
@@ -7,7 +7,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import client from "../../../libs/mongoConnect";
 
-const handler = NextAuth({
+export const authOptions = {
     secret: process.env.SECRET,
     adapter: MongoDBAdapter(client),
     providers: [
@@ -54,6 +54,8 @@ const handler = NextAuth({
         }
     },
     debug: true
-});
+}
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST }
--- food-order-app/src/app/profile/page.js
+++ food-order-app/src/app/profile/page.js
@@ -3,11 +3,27 @@
 import { useSession } from 'next-auth/react'
 import Image from 'next/image';
 import { redirect } from 'next/navigation';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import toast from "react-hot-toast";
 
 const page = () => {
     const session = useSession();
+    const [user, setUser] = useState(null);
+    const [userName, setUserName] = useState(session?.data?.user?.name || '');
     const { status } = session;
+
+    
+  useEffect(() => {
+    if (status === 'authenticated') {
+      fetch('/api/profile').then(response => {
+        response.json().then(data => {
+          setUser(data);
+          setIsAdmin(data.admin);
+          setProfileFetched(true);
+        })
+      });
+    }
+  }, [session, status]);
 
     if (status === 'loading') {
         return 'Loading...'
@@ -19,10 +35,32 @@ const page = () => {
 
     const userImage = session.data.user.image
 
+    const handleProfileInfoUpdate = async (e, data) => {
+        e.preventDefault();
+
+        const savingPromise = new Promise(async (resolve, reject) => {
+            const response = await fetch('/api/profile', {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data),
+            });
+            if (response.ok)
+                resolve()
+            else
+                reject();
+        });
+
+        await toast.promise(savingPromise, {
+            loading: 'Saving...',
+            success: 'Profile saved!',
+            error: 'Error',
+        });
+    }
+
     return (
         <section className='mt-8'>
             <h1 className="text-center text-primary text-4xl mb-4">Profile</h1>
-            <form className='max-w-md mx-auto'>
+            <div className='max-w-md mx-auto'>
                 <div className="flex gap-4 items-center">
                     <div>
                         <div className="p-2 rounded-lg flex flex-col items-center gap-2 w-46">
@@ -41,11 +79,12 @@ const page = () => {
                             </button>
                         </div>
                     </div>
-                    <div className="grow">
+                    <form className="grow" onSubmit={handleProfileInfoUpdate}>
                         <input
                             type="text"
                             placeholder="First and last name"
                             className="border p-2 rounded-md w-full mb-2"
+                            value={userName} onChange={e => setUserName(e.target.value)}
                         />
                         <input
                             type="email"
@@ -55,10 +94,10 @@ const page = () => {
                         <button type="submit" className="bg-blue-500 text-white py-1 px-4 rounded-md">
                             Save
                         </button>
-                    </div>
+                    </form>
                 </div>
 
-            </form>
+            </div>
         </section>
     )
 }
