a282a31be37fb53ce9588707551f536e2cd69279
TeodorTod <teodor.at.tod@gmail.com>
2024-10-04 16:09:44 +0300

    final

--- ng16-modules/src/app/app.component.ts
+++ ng16-modules/src/app/app.component.ts
@@ -1,49 +1,17 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ApiService } from './services/api.service';
-import { Observable } from 'rxjs';
+import { catchError, filter, finalize, from, map, mergeMap, Observable, of, take, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
+  source = from([1, 2, 3, 4, 5]);
+  doubled = this.source.pipe(filter(value => value % 2 !== 0));
 
-
-  constructor(private apiService: ApiService, private cdr: ChangeDetectorRef) {
-
+  constructor() {
+    this.doubled.subscribe(result => console.log(result));
   }
-
-  outputs: {
-    time: string,
-    message: string
-  }[] = [];
-
-  ngOnInit(): void {
-    this.createObservable()
-  }
-
-  createObservable() {
-    const observable = new Observable((subscriber) => {
-      subscriber.next(1);
-      subscriber.next(2);
-      subscriber.error('wrong');
-      subscriber.next(3);
-      subscriber.complete();
-    });
-
-
-    observable.subscribe({
-      next: (value: any) => {
-        console.log(value);
-      },
-      error: (error: any) => {
-        console.log(error);
-      },
-      complete: () => {
-        console.log('complete');
-      },
-    });
-  }
-
 }
