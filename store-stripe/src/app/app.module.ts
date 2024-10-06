75261175a16f3a83656e88f7c13c0f5684488104
TeodorTod <teodor.at.tod@gmail.com>
2024-10-06 22:00:12 +0300

    ui fixes

--- store-stripe/src/app/app.module.ts
+++ store-stripe/src/app/app.module.ts
@@ -21,12 +21,16 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
+import { FiltersComponent } from './pages/home/components/filters/filters.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    ProductsHeaderComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule,
--- store-stripe/src/app/pages/home/home.component.ts
+++ store-stripe/src/app/pages/home/home.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-
 })
 export class HomeComponent implements OnInit {
+  cols: number = 3;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onColumnsCountChange(colsNumber: number): void {
+    this.cols = colsNumber;
   }
 
 }
