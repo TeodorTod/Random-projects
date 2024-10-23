929a914268370510245675517fd4b3c8625b6521
TeodorTod <teodor.at.tod@gmail.com>
2024-10-23 17:11:03 +0300

    commit

--- angular-testing/src/app/app.component.ts
+++ angular-testing/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsComponent } from './components/reactive-forms/reactive-forms.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
---
+++ angular-testing/src/app/components/reactive-forms/reactive-forms.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-forms',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './reactive-forms.component.html',
+  styleUrl: './reactive-forms.component.scss'
+})
+export class ReactiveFormsComponent {
+
+  constructor(private formBuilder: FormBuilder) { }
+  profileForm = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.formBuilder.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: [''],
+    }),
+  })
+
+  onSubmit() {
+    console.log(this.profileForm);
+
+  }
+}
