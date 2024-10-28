1dc4ac6a3a18b0e917f4259d4c8bdd9f8ee3761c
TeodorTod <teodor.at.tod@gmail.com>
2024-10-28 21:27:31 +0200

    final

---
+++ responsive-menu/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, HostListener, OnInit, signal } from '@angular/core';
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
+import { MainComponent } from './main/main.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [LeftSidebarComponent, MainComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent implements OnInit {
+  isLeftSidebarCollapsed = signal<boolean>(false);
+  screenWidth = signal<number>(window.innerWidth);
+
+  ngOnInit(): void {
+    this.isLeftSidebarCollapsed.set(this.screenWidth() < 768);
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.screenWidth.set(window.innerWidth);
+    if (this.screenWidth() < 768) {
+      this.isLeftSidebarCollapsed.set(true);
+    }
+  }
+
+  changeIsLeftSidebarCollapsed(isLeftSidebarCollapsed: boolean): void {
+    this.isLeftSidebarCollapsed.set(isLeftSidebarCollapsed);
+  } 
+}
+
