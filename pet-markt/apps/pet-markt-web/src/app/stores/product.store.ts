e0bb6b19fbc3f971af4cad2edc9f94aea10089e0
TeodorTod <teodor.at.tod@gmail.com>
2025-06-25 21:21:17 +0300

    product store

--- pet-markt/apps/pet-markt-web/src/app/stores/product.store.ts
+++ pet-markt/apps/pet-markt-web/src/app/stores/product.store.ts
@@ -2,6 +2,20 @@ import { inject } from '@angular/core';
 import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
 import { Product } from '@prisma/client';
 import { Apollo, gql } from 'apollo-angular';
+import { tap } from 'rxjs';
+
+const GET_PRODUCTS = gql`
+  query GetProducts {
+    products {
+      id
+      name
+      description
+      price
+      image
+      stripePriceId
+    }
+  }
+`;
 
 export interface ProductState {
   products: Product[];
@@ -24,8 +38,22 @@ export const ProductStore = signalStore(
   withState(initialState),
   withMethods((store, apollo = inject(Apollo)) => ({
     loadProducts() {
-        patchState(store, { loading: true });
-        apollo.watchQuery
-    }
+      patchState(store, { loading: true });
+
+      apollo
+        .watchQuery<{ products: Product[] }>({
+          //@ts-ignore
+          query: GET_PRODUCTS,
+        })
+        .valueChanges.pipe(
+          tap({
+            next: ({ data }) =>
+              patchState(store, { products: data.products, loading: false }),
+            error: (error) =>
+              patchState(store, { error: error.message, loading: false }),
+          })
+        )
+        .subscribe();
+    },
   }))
 );
